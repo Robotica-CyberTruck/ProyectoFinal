fce75cd527fad8eff1ba9e0474630640354eaf14
Yesopi <y.pineros@uniandes.edu.co>
2023-06-05 11:11:48 -0500

    cositas

--- Robotics/build/robot_manipulator/prefix_override/sitecustomize.py
+++ Robotics/build/robot_manipulator/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/tatianam/ProyectoFinal/Robotics/install/robot_manipulator'
+sys.prefix = sys.exec_prefix = '/home/yesopi/ProyectoFinal/Robotics/install/robot_manipulator'

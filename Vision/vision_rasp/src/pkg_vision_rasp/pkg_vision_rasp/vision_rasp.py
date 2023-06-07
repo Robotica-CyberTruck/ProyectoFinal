a375bfa04048d0846a9a35112f7e159c9a364ed3
DanielHurtado98 <danielhurtado981215@gmail.com>
2023-06-06 19:02:40 -0500

    Martes 7pm

--- Vision/vision_rasp/src/pkg_vision_rasp/pkg_vision_rasp/vision_rasp.py
+++ Vision/vision_rasp/src/pkg_vision_rasp/pkg_vision_rasp/vision_rasp.py
@@ -8,8 +8,8 @@ class ImagePublisher(Node):
 
     def __init__(self):
         super().__init__('image_publisher')
-        self.publisher_ = self.create_publisher(Image, 'camera_image', 10)
-        self.timer_period = 0.01  # segundos
+        self.publisher_ = self.create_publisher(Image, 'camera_image', 5)
+        self.timer_period = 0.001  # segundos
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
         self.i = 0
         self.bridge = CvBridge()
